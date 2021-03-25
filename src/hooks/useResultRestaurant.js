281404deabe95a8ebc2910c0afde1a9e7dfdfaef
mmajlesi <52847723+mmajlesi@users.noreply.github.com>
2021-03-25 19:56:52 +0430

    useResultRestaurant.js ["Create , Extraction Hook from SearchScreen and useEffect for run one time after render component."]

---
+++ src/hooks/useResultRestaurant.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import yelp from '../api/yelp';
+
+
+export default () => {
+    const [resultsRestaurants, setResult] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const searchApi = async (searchTerm) => {
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: searchTerm,
+                    location: 'san jose'
+                }
+            });
+            setResult(response.data.businesses);
+            setErrorMessage('');
+        } catch (e) {
+            console.log('error');
+            setErrorMessage('Something went wrong!');
+        }
+    };
+    useEffect(() => { searchApi('pasta'); }
+        , []);
+
+    return [searchApi, resultsRestaurants, errorMessage];
+}
--- src/screen/SearchScreen.js
+++ src/screen/SearchScreen.js
@@ -1,34 +1,18 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResultRestaurant from '../hooks/useResultRestaurant';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
-    const [resultsRestaurants, setResult] = useState([]);
-    const [errorMessage, setErrorMessage] = useState('');
-    const searchApi = async () => {
-        try {
-            const response = await yelp.get('/search', {
-                params: {
-                    limit: 50,
-                    term,
-                    location: 'san jose'
-                }
-            });
-            setResult(response.data.businesses);
-            setErrorMessage('');
-        } catch (e) {
-            console.log('error');
-            setErrorMessage('Something went wrong!');
-        }
-    }
+    const [searchApi, resultsRestaurants, errorMessage] = useResultRestaurant();
+
     return (
         <View>
             <SearchBar
                 term={term}
                 onTermChange={setTerm}
-                onTermSubmit={searchApi}
+                onTermSubmit={() => searchApi(term)}
             />
             <Text>We have found {resultsRestaurants.length} restaurant.</Text>
             {errorMessage ? <Text>Error -&gt; {errorMessage}</Text> : null}
